ca0543014dca3d603f8a134f3453173ae89fcc7b
alemr214 <ale.mr0214@gmail.com>
2025-06-29 01:02:06 -0600

    Add variables for full name construction using template literals

---
+++ 02-intro-javascript/src/bases/01-const-let.jsx
@@ -0,0 +1,8 @@
+// Variables
+
+const nombre = "Alejandro";
+const apellido = "Martinez";
+
+// const nombreCompleto = nombre + " " + apellido;
+const nombreCompleto = `${nombre} ${apellido}`;
+console.log(nombreCompleto);
