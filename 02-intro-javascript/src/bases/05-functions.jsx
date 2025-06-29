0fb74b83edc7386e757f7bc7a1aba8f062960dcb
alemr214 <ale.mr0214@gmail.com>
2025-06-29 01:02:41 -0600

    Add arrow function example for greeting in JavaScript

---
+++ 02-intro-javascript/src/bases/05-functions.jsx
@@ -0,0 +1,13 @@
+// function saludar( nombre ){
+//     return `Hola, ${nombre}`;
+// };
+
+// const saludar = function (nombre) {
+//     return `Hola, ${nombre}`;
+// }
+
+const saludar = (nombre) => {
+    return `Hola, ${nombre}`;
+}
+
+console.log(saludar('Mundo'));
