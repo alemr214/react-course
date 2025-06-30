0d87fbcc3aa7f4b323ceba9c5a38851fdcd5993d
alemr214 <ale.mr0214@gmail.com>
2025-06-30 00:34:30 -0600

    Refactor FirstApp component to use props for title and subtitle, and add prop-types validation

--- 03-counter-app/src/FirstApp.jsx
+++ 03-counter-app/src/FirstApp.jsx
@@ -1,10 +1,23 @@
-const FirstApp = () => {
+import PropTypes from "prop-types";
+
+const getName = (name) => {
+    return name;
+};
+
+const FirstApp = ({ title, subTitle }) => {
     return (
         <>
-            <h1>First App</h1>
+            <h1>{getName("Alejandro")}</h1>
             <h2>Soy un subtitulo</h2>
+            <h3>{title}</h3>
+            <h4>{subTitle}</h4>
         </>
     );
+};
+
+FirstApp.propTypes = {
+    title: PropTypes.string.isRequired,
+    subTitle: PropTypes.string.isRequired,
 };
 
 export default FirstApp;
