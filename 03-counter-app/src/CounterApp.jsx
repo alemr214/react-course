8a01ff396ff117b583a85efe047b432877234ab0
alemr214 <ale.mr0214@gmail.com>
2025-08-08 12:51:45 -0600

    Refactor CounterApp to manage internal state for counter; update initial value in main.jsx

--- 03-counter-app/src/CounterApp.jsx
+++ 03-counter-app/src/CounterApp.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const CounterApp = ({ value }) => {
+    const [counter, setCounter] = useState(value);
+
+    const handleAdd = () => {
+        setCounter(counter + 1);
+    };
+    const handleSubstract = () => {
+        setCounter(counter - 1);
+    };
+    const handleReset = () => {
+        setCounter(value);
+    };
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
+            <button onClick={handleAdd}>Click to +1</button>
+            <button onClick={handleSubstract}>Click to -1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 };
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -7,6 +7,6 @@ import "./styles/styles.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <CounterApp value={1234} />
+        <CounterApp value={0} />
     </React.StrictMode>
 );
